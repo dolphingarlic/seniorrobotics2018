9bbcd761aa06eddfa4baee0d4973d02af08956d2
dolphingarlic <dolphingarlic@gmail.com>
2018-05-29 15:52:11 +0200

    Added forwards function

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,25 @@
 from ev3dev.auto import *
 import time
 
-def run_motor(output,time,speed):
-    if output=="A":
+def run_motor(output, time, speed):
+    if output == "A":
         m = Motor(OUTPUT_A)
-    if output=="B":
+    elif output == "B":
         m = Motor(OUTPUT_B)
-    if output=="C":
+    elif output == "C":
         m = Motor(OUTPUT_C)
-    if output=="D":
+    elif output == "D":
         m = Motor(OUTPUT_D)
 
     m.run_timed(time_sp=time, speed_sp=speed)
 
 def get_colour():
     cs = ColorSensor()
-    return cs.raw
+    return cs.value;
 
+def forwards():
+    m_a = Motor(OUTPUT_A)
+    m_d = Motor(OUTPUT_D)
+
+    m_a.run_direct(time = 1000000, speed = 500)
+    m_d.run_direct(time = 1000000, speed = 500)
