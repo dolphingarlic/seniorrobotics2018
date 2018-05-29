a6a6197f16ac30fc5b65c77cf37078e5547e8883
dolphingarlic <dolphingarlic@gmail.com>
2018-05-29 16:12:08 +0200

    Test time

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,4 @@
 from ev3dev.auto import *
-import asyncio
-from concurrent.futures import ProcessPoolExecutor
 
 def run_motor(output,time,speed):
     # use as follows:
@@ -22,10 +20,6 @@ def get_colour():
     cs = ColorSensor()
     return cs.raw
 
-def forwards():
-    executor = ProcessPoolExecutor(2)
-    loop = asyncio.get_event_loop()
-    m_1 = asyncio.ensure_future(loop.run_in_executor(executor, run_motor(OUTPUT_A, 1000, 500)))
-    m_2 = asyncio.ensure_future(loop.run_in_executor(executor, run_motor(OUTPUT_D, 1000, 500)))
-
-    loop.run_forever()
+def forwards(time, speed):
+    run_motor("A", time, speed)
+    run_motor("D", time, speed)
