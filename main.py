4b019fe2f7af18d982bf3c3ee495069350110e8f
William Moolman <moolman.william@gmail.com>
2018-05-29 16:08:08 +0200

    Test dual running motors

--- main.py
+++ main.py
@@ -3,5 +3,5 @@
 import src.stage_1 as stage_1
 import src.utils as utils
 
-utils.run_motor("A", 100000, 1)
-utils.run_motor("D", 100000, 1)
+utils.run_motor("A", 100000, 100)
+utils.run_motor("D", 100000, 100)
