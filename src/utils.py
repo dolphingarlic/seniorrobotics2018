ee315c13903ba20d0a393c15b3a11aba1aabf721
dolphingarlic <dolphingarlic@gmail.com>
2018-05-29 15:57:01 +0200

    Test time

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,6 @@ from ev3dev.auto import *
 import time
 
 def run_motor(output,time,speed):
-    # use as follows:
-    # ouput = "A"/"B"/"C"/"D"
-    # time = 1000x where x is no of seconds
-    # speed = 0.5y where y is rpm)
     if output=="A":
         m = Motor(OUTPUT_A)
     if output=="B":
@@ -21,3 +17,9 @@ def get_colour():
     cs = ColorSensor()
     return cs.raw
 
+def forwards():
+    m_1 = Motor(OUTPUT_A)
+    m_2 = Motor(OUTPUT_D)
+
+    m_1.run_direct(time = 1000000, speed = 500)
+    m_2.run_direct(time = 1000000, speed = 500)
