22539fe8b98c655f3f3cd1d83f41236f5d704496
dolphingarlic <dolphingarlic@gmail.com>
2018-05-29 16:24:24 +0200

    Test time

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+from ev3dev.ev3 import *
 import src.stage_1 as stage_1
 import src.utils as utils
 
-utils.run_motor("A", 100000, 100)
-utils.run_motor("D", 100000, 100)
+motor_a = Motor(OUTPUT_A)
+
+utils.run_motor("A", 1000, 1000)
+utils.run_motor("D", 1000, 1000)
+motor_a.wait_while('running')
 
 utils.lift()
-utils.place()
