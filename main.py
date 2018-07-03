d4ab164a0d9511145b4113c69c2e8fbc927d2878
William Moolman <moolman.william@gmail.com>
2018-07-03 16:14:57 +0200

    added grabber functionality

--- main.py
+++ main.py
@@ -3,3 +3,7 @@
 from ev3dev.ev3 import *
 import src.stage_1 as stage_1
 import src.utils as utils
+#initializes motor objects, change outputs based on ports blugged into
+utils.grab()
+utils.release_grabber()
+
--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,28 @@
 from ev3dev.auto import *
 
-def run_motor(output,time,speed):
+wheel1 = Motor(OUTPUT_A)
+wheel2 = Motor(OUTPUT_B)
+grabber = Motor(OUTPUT_C)
+claw = Motor(OUTPUT_D)
+
+def run_motor(motor,time,speed):
     '''
     Use as follows:
     ouput = "A"/"B"/"C"/"D"
     time = 1000x where x is no of seconds
     speed = 5y where y is rpm
     '''
-    if output=="A":
-        m = Motor(OUTPUT_A)
-    if output=="B":
-        m = Motor(OUTPUT_B)
-    if output=="C":
-        m = Motor(OUTPUT_C)
-    if output=="D":
-        m = Motor(OUTPUT_D)
 
-    m.run_timed(time_sp=time, speed_sp=speed)
+    motor.run_timed(time_sp=time, speed_sp=speed)
+
+def grab():
+    run_motor(grabber, 1000, 1000)
+
+
+def release_grabber():
+    run_motor(grabber, 1000, -1000)
+
+
     
 def get_colour1():                                  #For a colour sensor in port 1
     cs1 = ColorSensor(address="1")
@@ -27,8 +33,8 @@ def get_colour2():                                  #For a colour sensor in port
     return cs2.color
 
 def forwards(time, speed):
-    run_motor("A", time, speed)
-    run_motor("D", time, speed)
+    run_motor(wheel1, time, speed)
+    run_motor(wheel2, time, speed)
 
 def lift():
     lifter = MediumMotor('outB')
