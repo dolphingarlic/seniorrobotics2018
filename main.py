3bb09ae0a6c622f30ac2c07a1dbf9d0929a93666
dolphingarlic <dolphingarlic@gmail.com>
2018-07-27 17:23:57 +0200

    Made the Robot class

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
-from ev3dev.ev3 import *
-import src.stage_1 as stage_1
-import src.utils as utils
-#initializes motor objects, change outputs based on ports blugged into
-#utils.grab()
-utils.release_grabber()
+from src.robot import Robot
 
+robot = Robot()
+robot.move_forwards(1000, 1000)
+robot.turn("LEFT")
---
+++ src/robot.py
@@ -0,0 +1,46 @@
+"""A simple Robot class for the EV3 robots"""
+
+from ev3dev.auto import *
+
+
+class Robot(object):
+    """A Robot class that emulates the EV3 robots
+
+    This is a Robot class created for the SJC Robotics team
+    to try to emulate the EV3 robots' non-Python capabilities
+    like moving forwards, getting the colour etc.
+
+    Attributes:
+        grabber: The grabbing claw that picks up the lego 'food' block
+        colour_sensor1: Colour Sensor in port 1
+        colour_sensor2: Colour Sensor in port 4
+        left_wheel: The motor for the left wheel
+        right_wheel: The motor for the right wheel
+    """
+
+    def __init__(self):
+        self.grabber = MediumMotor('outB')
+        self.colour_sensor1 = ColorSensor(address='1')
+        self.colour_sensor2 = ColorSensor(address='4')
+        self.left_wheel = Motor(OUTPUT_A)
+        self.right_wheel = Motor(OUTPUT_B)
+
+    def move_forwards(self, time, speed):
+        """Moves the robot straight forwards for a given time"""
+        self.left_wheel.runTimed(time_sp=time, speed_sp=speed)
+        self.right_wheel.runTimed(time_sp=time, speed_sp=speed)
+
+    def turn(self, direction):
+        """Turns the robot 90 degrees in a given direction"""
+        if direction.upper() == 'LEFT':
+            self.left_wheel.run_timed(time_sp=1000, speed_sp=1000)
+        elif direction.upper() == 'RIGHT':
+            self.right_wheel.run_timed(time_sp=1000, speed_sp=1000)
+
+    def grab(self):
+        """Makes the grabber grab the food brick"""
+        self.grabber.run_to_rel_pos(position_sp=-540, speed_sp=1000)
+
+    def release(self):
+        """Makes the grabber release the food brick"""
+        self.grabber.run_to_rel_pos(position_sp=540, speed_sp=1000)
