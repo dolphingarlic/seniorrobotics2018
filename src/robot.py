adceb0b48827510f8a495707ca3434cc48472abb
Andi Qu <31325319+dolphingarlic@users.noreply.github.com>
2018-07-30 14:53:37 +0200

    Update robot.py

--- src/robot.py
+++ src/robot.py
@@ -14,39 +14,44 @@ class Robot(object):
         grabber: The grabbing claw that picks up the lego 'food' block
         left_colour_sensor: Colour Sensor in port 1
         right_colour_sensor: Colour Sensor in port 4
+        inner_colour_sensor: Colour Sensor that senses box colour
+        outer_colour_sensor: Colour Sensor that senses ship colour
         left_wheel: The motor for the left wheel
         right_wheel: The motor for the right wheel
     """
-    rotation = "S"
+    rotation = 'S'
+    compass = {'N': 0, 'E': 90, 'S': 180, 'W': -90}
+    
+    COLORS = {2: 'BLUE', 3: 'GREEN', 4: 'YELLOW', 5: 'RED'}
 
-    def __init__(self):
+        def __init__(self):
         self.grabber = Motor(OUTPUT_C)
         self.arm = Motor(OUTPUT_B)
         self.left_colour_sensor = ColorSensor(address='3')
         self.right_colour_sensor = ColorSensor(address='2')
-        self.inner_colour_sensor = ColorSensor(address='2')
-        self.outer_colour_sensor = ColorSensor(address='3')
+        self.inner_colour_sensor = ColorSensor(address='1')
+        self.outer_colour_sensor = ColorSensor(address='4')
         self.left_wheel = Motor(OUTPUT_D)
         self.right_wheel = Motor(OUTPUT_A)
 
-    def move_straight(self, duration, speed, direction):
+        def move_straight(self, move_time, speed, direction):
         """Moves the robot straight for a given time"""
 
-        if direction.upper() == 'FORWARDS':
-            self.left_wheel.run_timed(time_sp=duration, speed_sp=speed)
-            self.right_wheel.run_timed(time_sp=duration, speed_sp=speed)
-        elif direction.upper() == 'BACKWARDS':
-            self.left_wheel.run_timed(time_sp=duration, speed_sp=-speed)
-            self.right_wheel.run_timed(time_sp=duration, speed_sp=-speed)
+        if direction.upper() == 'BACKWARDS':
+            self.left_wheel.run_timed(time_sp=move_time, speed_sp=speed)
+            self.right_wheel.run_timed(time_sp=move_time, speed_sp=speed)
+        elif direction.upper() == 'FORWARDS':
+            self.left_wheel.run_timed(time_sp=move_time, speed_sp=-speed)
+            self.right_wheel.run_timed(time_sp=move_time, speed_sp=-speed)
 
     # def follow_black_line(self):
 
     def turn(self, direction):
         """Turns the robot 90 degrees in a given direction"""
         if direction.upper() == 'LEFT':
-            self.left_wheel.run_to_rel_pos(position_sp=360, speed_sp=1000)
+            self.left_wheel.run_to_rel_pos(position_sp=720, speed_sp=500)
         elif direction.upper() == 'RIGHT':
-            self.right_wheel.run_to_rel_pos(position_sp=360, speed_sp=1000)
+            self.right_wheel.run_to_rel_pos(position_sp=720, speed_sp=500)
 
     def grab(self):
         """Makes the grabber grab the food brick"""
@@ -56,10 +61,9 @@ class Robot(object):
         """Makes the grabber release the food brick"""
         self.grabber.run_to_rel_pos(position_sp=540, speed_sp=1000)
 
-    def scan(self):
-        return self.outer_colour_sensor.color()
-
-    compass = {"N": 0, "E": 90, "S": 180, "W": -90}
+    def scan(self, colour_sensor):
+        """Returns the colour that the color sensor senses"""
+        return colour_sensor.color
 
     def move_to(self, arr):
         for i in arr-1:
