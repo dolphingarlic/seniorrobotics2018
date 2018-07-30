0e7347ffe15ca675d75408b409deb3ef696e7c3b
dolphingarlic <dolphingarlic@gmail.com>
2018-07-30 13:22:30 +0200

    Main

--- main.py
+++ main.py
@@ -4,14 +4,10 @@ from time import sleep
 from src.robot import Robot
 
 robot = Robot()
-robot.move_straight(1000, 1000, 'Forwards')
+robot.move_straight(10000, 1000, 'Forwards')
 robot.left_wheel.wait_until_not_moving()
-robot.turn('Left')
-robot.left_wheel.wait_until_not_moving()
+robot.right_wheel.wait_until_not_moving()
 robot.grab()
 robot.grabber.wait_until_not_moving()
+sleep(2)
 robot.release()
-
-for i in range(100):
-    print(robot.scan(robot.outer_colour_sensor))
-    sleep(0.5)
--- src/robot.py
+++ src/robot.py
@@ -28,15 +28,15 @@ class Robot(object):
         self.left_wheel = Motor(OUTPUT_D)
         self.right_wheel = Motor(OUTPUT_A)
 
-    def move_straight(self, time, speed, direction):
+    def move_straight(self, move_time, speed, direction):
         """Moves the robot straight for a given time"""
 
         if direction.upper() == 'BACKWARDS':
-            self.left_wheel.run_timed(time_sp=time, speed_sp=speed)
-            self.right_wheel.run_timed(time_sp=time, speed_sp=speed)
+            self.left_wheel.run_timed(time_sp=move_time, speed_sp=speed)
+            self.right_wheel.run_timed(time_sp=move_time, speed_sp=speed)
         elif direction.upper() == 'FORWARDS':
-            self.left_wheel.run_timed(time_sp=time, speed_sp=-speed)
-            self.right_wheel.run_timed(time_sp=time, speed_sp=-speed)
+            self.left_wheel.run_timed(time_sp=move_time, speed_sp=-speed)
+            self.right_wheel.run_timed(time_sp=move_time, speed_sp=-speed)
 
     #def follow_black_line(self):
 
