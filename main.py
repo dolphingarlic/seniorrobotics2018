6f35afdb3f78299d603ba399249c83cd1843670c
William Moolman <moolman.william@gmail.com>
2018-08-01 12:16:55 +0200

    Testing code

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ from time import sleep
 from src.robot import Robot
 from src.map_nodes import *
 
+
 def take_lid_and_place_box(robot):
     robot.lift()
     robot.move_straight_degrees(235, 500)
     robot.move_straight_degrees(-235, 500)
 
+
 ROBOT = Robot()
 
-for i in range(50):
-    print("Right: "+str(ROBOT.right_colour_sensor.reflected_light_intensity))
-    print("Left: "+str(ROBOT.left_colour_sensor.reflected_light_intensity))
-    sleep(0.5)
+ROBOT.follow_until_next_node_p()
 
