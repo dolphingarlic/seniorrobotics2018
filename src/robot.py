b4ff59b1cefce7a8562a431d8a344ca6626c696d
William Moolman <moolman.william@gmail.com>
2018-08-02 10:04:36 +0200

    IDK, implemeented cahnging duty cycle sp independently againg, this shit better work

--- src/robot.py
+++ src/robot.py
@@ -91,20 +91,19 @@ class Robot(object):
                         and time_start > time()+50:
                     print("stop")
                 else:
-                    self.right_colour_sensor.duty_cycle_sp = 80
-                    self.left_wheel.run_direct(duty_cycle_sp=self.steering(
-                        self.left_colour_sensor.reflected_light_intensity - 30) * 1.5)
+                    self.left_wheel.duty_cycle_sp = self.steering(
+                        self.left_colour_sensor.reflected_light_intensity - 30) * 1.5
                     print("Left: "+str(self.steering(
                         self.left_colour_sensor.reflected_light_intensity - 30) * 1.5))
             else:
                 if self.right_colour_sensor.reflected_light_intensity < self.PROPORTIONAL_THRESHOLD:
-                    self.right_wheel.run_direct(duty_cycle_sp=self.steering(
-                        self.right_colour_sensor.reflected_light_intensity - 30) * 1.5)
+                    self.left_wheel.duty_cycle_sp = self.steering(
+                        self.right_colour_sensor.reflected_light_intensity - 30) * 1.5
                     print("Right: "+str(self.steering(
                         self.right_colour_sensor.reflected_light_intensity - 30) * 1.5))
                 else:
-                    self.left_wheel.run_direct(duty_cycle_sp=80)
-                    self.right_wheel.run_direct(duty_cycle_sp=80)
+                    self.right_wheel.duty_cycle_sp = 80
+                    self.left_wheel.duty_cycle_sp = 80
                     print("Straight")
 
     def follow_black_line_degrees(self, degrees, speed, direction):
