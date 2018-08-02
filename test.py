1f990dc077d2ebcf2025215e103c187c59b41fbb
William Moolman <moolman.william@gmail.com>
2018-08-02 14:39:19 +0200

    wut

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ from src.robot import Robot
 from time import sleep
 ROBOT = Robot()
 print("Started")
-'''
+
 ROBOT.follow_until_next_node()
 sleep(10)
 ROBOT.stop()
 print("Stopped")
-'''
+
 '''
 for i in range(40):
     print("L:"+str(ROBOT.left_colour_sensor.reflected_light_intensity))
@@ -19,8 +19,9 @@ print("Right: "+str(ROBOT.right_colour_sensor.reflected_light_intensity))
 print("Inner: "+str(ROBOT.inner_colour_sensor.reflected_light_intensity))
 print("Outer: "+str(ROBOT.outer_colour_sensor.reflected_light_intensity))
 '''
-
+'''
 for i in range(1):
     print("Left: " + str(ROBOT.left_colour_sensor.reflected_light_intensity))
     print("Right: " + str(ROBOT.right_colour_sensor.reflected_light_intensity))
     sleep(0.2)
+'''
